000d0b7c4fae2a5f39c8e0d3fe5a59a7ee7755b3
The-Blight <hezer.fil@gmail.com>
2026-03-10 15:34:53 +0300

    first commit

---
+++ Games_WebAPI/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+const string version = "v1";
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/openapi/{version}.json", version); });
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
+
